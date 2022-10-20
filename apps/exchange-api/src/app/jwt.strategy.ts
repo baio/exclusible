b2cde6c81148de97cb7ce6873943df6bd7d5ff2a
baio <max.putilov@gmail.com>
2022-10-20 10:22:41 +0100

    auth config

--- apps/exchange-api/src/app/jwt.strategy.ts
+++ apps/exchange-api/src/app/jwt.strategy.ts
@@ -3,28 +3,29 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { passportJwtSecret } from 'jwks-rsa';
 
+const AUTH0_ISSUER_URL = process.env.AUTH0_ISSUER_URL;
+
 // TODO : Pass params through config !
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
-    console.log('wtf ???');
+    console.log('wtf ???', AUTH0_ISSUER_URL);
     super({
       secretOrKeyProvider: passportJwtSecret({
         cache: false,
         rateLimit: false,
         jwksRequestsPerMinute: 5,
-        jwksUri: 'https://baio.eu.auth0.com/.well-known/jwks.json',
+        jwksUri: `${AUTH0_ISSUER_URL}.well-known/jwks.json`,
       }),
 
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      audience: `https://baio.eu.auth0.com/api/v2/`,
-      issuer: 'https://baio.eu.auth0.com/',
+      audience: `${AUTH0_ISSUER_URL}api/v2/`,
+      issuer: AUTH0_ISSUER_URL,
       algorithms: ['RS256'],
     });
   }
 
   validate(payload: unknown): unknown {
-    console.log('222', payload);
     return payload;
   }
 }
